df7944e86f732e2d81d92aca48ecac1fc94ef899
juilleratn <juilleratnoel@gmail.com>
2019-10-07 16:54:59 +0200

    début de la mise en place de l'affichage de la recette
    Signed-off-by: juilleratn <juilleratnoel@gmail.com>

---
+++ src/AppBundle/Controller/AffichageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AffichageController extends Controller
+{
+    /**
+     * Affichage controller.
+     *
+     * @Route("/affichage")
+     */
+    public function indexAction()
+    {
+
+    }
+}
