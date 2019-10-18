7c78d5035b8399efe85bee84a22be883539a56cf
juilleratn <juilleratnoel@gmail.com>
2019-10-18 16:26:51 +0200

    18/10
    Signed-off-by: juilleratn <juilleratnoel@gmail.com>

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,25 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+//    /**
+//     * Export to PDF
+//     *
+//     * @Route("/pdf", name="acme_demo_pdf")
+//     */
+//    public function pdfAction()
+//    {
+//        $html = $this->renderView('AppBundle:Demo:pdf.html.twig');
+//
+//        $filename = sprintf('test-%s.pdf', date('Y-m-d'));
+//
+//        return new Response(
+//            $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+//            200,
+//            [
+//                'Content-Type'        => 'application/pdf',
+//                'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
+//            ]
+//        );
+//    }
 }
